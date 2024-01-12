8a56aa1fd6eccdba88e9db1a4a5bef4dc3beb6cd
Iris Felshman <iris.felshman@gmail.com>
2024-01-12 16:05:40 -0600

    Update CruftRemover.ps1
    moving comments out of it, adding one more explanatory note as well though.

--- CruftRemover.ps1
+++ CruftRemover.ps1
@@ -1,13 +1,5 @@
-# Script for uninstalling unnecessary applications from Windows on ARM 4GB laptops
-# Please feel free to add or remove programs based on your own preferences.
-# Some inspiration taken from tiny11: https://github.com/ntdevlabs/tiny11builder
-# To see current installed programs on your machine, run this one-liner in PowerShell Admin:
-# Get-AppxPackage | SELECT Name
-# Microsoft.549981C3F5F10 is Cortana.
-# This uninstalls a bunch of stuff. If you like stuff I am removing, feel free to remove it from the script before you run it.
-# Things that I am not uninstalling because Windows errors at me:
-# "Microsoft.XboxSpeechToTextOverlay","Microsoft.XboxGameCallableUI"
-# I am actively using this on my laptop but this could possibly break your install maybe! No promises!
+# Script for uninstalling unnecessary applications from Windows 4GB Laptops.
+# Run this in PowerShell as an Administrator to use.
 $PackageList = @("Microsoft.YourPhone",
 "Microsoft.GetHelp",
 "Microsoft.GetStarted",
